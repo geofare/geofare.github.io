95f53a7dd689e1e2aa8dfabec881f316ff2529b2
Luke Trenaman <trenamanluke@gmail.com>
2022-03-26 16:40:46 -0400

    This is our map

--- leaflet-test/index.js
+++ leaflet-test/index.js
@@ -1,8 +1,31 @@
+let data = [
+    {
+        "address":"1415 Rhoadmiller St, Richmond, VA 23220",
+        "latitude":37.56651,
+        "longitude":-77.45572,
+        "name":"Feed More",
+        "description":"You can get food here on 3/27/2022, from 6pm-8pm."
+    },
+    {
+        "address":"1021 Oliver Hill Way, Richmond, VA 23219",
+        "latitude":37.542561,
+        "longitude":-77.423912,
+        "name":"Capital Area Partnership Uplifting People (CAPUP)",
+        "description":"You can get food here on 3/28/2022, from 5pm-7pm"
+    },
+    {
+        "address":"400 Chimborazo Blvd, Richmond, VA 23223",
+        "latitude":37.5279,
+        "longitude":-77.4099,
+        "name":"Masjid Bilal",
+        "description":"You can get food here on 3/27/2022, from 3pm-5pm"
+    }
+]
 window.onload = function(){
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([37.56651, -77.45572], 13);
     icon = L.divIcon({
         className: 'custom-div-icon',
-            html: "<div style='background-color:#4838cc;' class='marker-pin'></div><i class='fa fa-camera'></i>",
+            html: "<div style='background-color:#4838cc;' class='marker-pin'></div><i class='fa-solid fa-wheat-awn'></i>",
             iconSize: [30, 42],
             iconAnchor: [15, 42]
         });
@@ -16,16 +39,27 @@ window.onload = function(){
         zoomOffset: -1,
         accessToken: 'pk.eyJ1IjoibHVrZXRyZW5hbWFuIiwiYSI6ImNsMTg4NmduZzA5cGszYnVsYmM1MjZ0d2UifQ.sIOH_Wp8owixptV9YIau4g'
     }).addTo(map);
-    var marker = L.marker([51.5, -0.09],{
-        title:"Kroger's Grocery Store",
-        "icon":icon
-    }).addTo(map);
-    var popup = L.popup({
-        closeButton: false,
-        autoClose: false
-      })
-      .setLatLng([39.7392, -104.9903])
-      .setContent('<h1>Text box on a map</h1>');
-    marker.bindPopup(popup);
+    function generateLocation(obj){
+        console.log(obj);
+        var marker = L.marker([obj.latitude, obj.longitude],{
+            title:"Kroger's Grocery Store",
+            "icon":icon
+        }).addTo(map);
+        var popup = L.popup({
+            closeButton: false,
+            autoClose: false
+          })
+          .setLatLng([obj.latitude, obj.longitude])
+          .setContent(`
+          <div><h1>${obj.name}</h1>
+            <p>You are able to get food at this location.</p>
+            <p>${obj.description}</p>
+            <p>Click <a target = "_blank" href="https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(obj.address)}">here</a> for direcitons.</p>
+          </div>`);
+        marker.bindPopup(popup);
+    }
+    data.forEach(function(val){
+        generateLocation(val);
+    })
     
 }

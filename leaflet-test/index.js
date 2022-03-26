05ee4fe7c24de8b7c6bf62e919f0263ecc4f1c5f
Luke Trenaman <trenamanluke@gmail.com>
2022-03-26 15:46:00 -0400

    data

--- leaflet-test/index.js
+++ leaflet-test/index.js
@@ -2,7 +2,7 @@ window.onload = function(){
     var map = L.map('map').setView([51.505, -0.09], 13);
     icon = L.divIcon({
         className: 'custom-div-icon',
-            html: "<div style='background-color:#4838cc;' class='marker-pin'></div><i class='fa-solid fa-camera'></i>",
+            html: "<div style='background-color:#4838cc;' class='marker-pin'></div><i class='fa fa-camera'></i>",
             iconSize: [30, 42],
             iconAnchor: [15, 42]
         });
@@ -20,5 +20,12 @@ window.onload = function(){
         title:"Kroger's Grocery Store",
         "icon":icon
     }).addTo(map);
-    marker.bindPopup("This is the Transamerica Pyramid").openPopup();
+    var popup = L.popup({
+        closeButton: false,
+        autoClose: false
+      })
+      .setLatLng([39.7392, -104.9903])
+      .setContent('<h1>Text box on a map</h1>');
+    marker.bindPopup(popup);
+    
 }
